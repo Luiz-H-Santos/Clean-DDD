5053f2b0a376c58d990c421ae3e9a2405d8d16fe
Luiz Henrique <luiz.h.santos.dev@gmail.com>
2026-05-07 21:11:37 -0300

    refactor: implement Props pattern for Answer and Question entities

--- src/domain/use-cases/answer-questions.ts
+++ src/domain/use-cases/answer-questions.ts
@@ -11,7 +11,11 @@ export class AnswerQuestionUseCase {
   execute({instructorId, questionId, content} : AnswerQuestionUseCaseRequest) {
  
 
-    const answer = new Answer(content)
+    const answer = new Answer({
+      content, 
+      authorId: instructorId, 
+      questionId
+    })
 
     return answer
 
