79a65693bb4cfb1c6e4f167a76fbd152ab837179
Luiz Henrique <luiz.h.santos.dev@gmail.com>
2026-05-10 19:07:27 -0300

    feat(domain): add slug value obejct and tests

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,13 +1,17 @@
 import { randomUUID } from "node:crypto"
+import { Slug } from "./value-objects/slug"
 
 interface QuestionProps {
+
   title: string
   content: string
+  slug: Slug
   authorId: string
 }
 export class Question {
   public id: string
   public title: string
+  public slug: Slug
   public content: string
   public authorId: string
 
@@ -15,6 +19,7 @@ export class Question {
     this.title = props.title
     this.content  = props.content
     this.id = id ?? randomUUID()
-    this.authorId =props.authorId
+    this.authorId = props.authorId
+    this.slug = props.slug
   }
 }
