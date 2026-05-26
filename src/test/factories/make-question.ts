5782ebadc497d7c202113b47790fb8ca953ed6a3
Luiz Henrique <luiz.h.santos.dev@gmail.com>
2026-05-25 21:36:21 -0300

    test(factory): use faker to generete fake question data

--- src/test/factories/make-question.ts
+++ src/test/factories/make-question.ts
@@ -1,3 +1,4 @@
+import { faker } from '@faker-js/faker'
 import { UniqueEntityId } from '@/core/entities/unique-entity-id.js'
 import {
   Question,
@@ -5,14 +6,20 @@ import {
 } from '@/domain/forum/enterprise/entities/question.js'
 import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug.js'
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    authorId: new UniqueEntityId(),
-    title: 'Example question',
-    slug: Slug.create('example-question'),
-    content: 'Example content',
-    ...override,
-  })
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityId,
+) {
+  const question = Question.create(
+    {
+      authorId: new UniqueEntityId(),
+      title: faker.lorem.sentence(),
+      slug: Slug.create('example-question'),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  )
 
   return question
 }
