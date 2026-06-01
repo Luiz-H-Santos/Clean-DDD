0ced46e547f0bbaba3c34dbb2f19b66026d47b7d
Luiz Henrique <luiz.h.santos.dev@gmail.com>
2026-06-01 05:28:10 -0300

    feat(questions): implement delete question use case and tests

--- src/test/repositores/in-memory-question-repository.ts
+++ src/test/repositores/in-memory-question-repository.ts
@@ -4,6 +4,15 @@ import { Question } from '@/domain/forum/enterprise/entities/question.js'
 export class InMemoryQuestionRepository implements QuestionRepository {
   public items: Question[] = []
 
+  async findById(id: string) {
+    const question = this.items.find((item) => item.id.toString() === id)
+
+    if (!question) {
+      return null
+    }
+    return question
+  }
+
   async findBySlug(slug: string) {
     const question = this.items.find((item) => item.slug.value === slug)
 
@@ -15,5 +24,11 @@ export class InMemoryQuestionRepository implements QuestionRepository {
 
   async create(question: Question) {
     this.items.push(question)
+  }
+
+  async delete(question: Question) {
+    const itemIndex = this.items.findIndex((item) => item.id === question.id)
+
+    this.items.splice(itemIndex, 1)
   }
 }
