9f5171111c630f2ea19d35524e955c1bb7d40463
Luiz Henrique <luiz.h.santos.dev@gmail.com>
2026-06-01 06:00:10 -0300

    feat(answaers): implement delete answer use case and tests

--- src/domain/forum/application/repositories/answers-repository.ts
+++ src/domain/forum/application/repositories/answers-repository.ts
@@ -2,4 +2,6 @@ import { Answer } from '../../enterprise/entities/answer.js'
 
 export interface AnswersRepository {
   create(answer: Answer): Promise<void>
+  findById(id: string): Promise<Answer | null>
+  delete(answer: Answer): Promise<void>
 }
