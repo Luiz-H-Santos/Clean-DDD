d5d647911b9331f670e95551c7b1fda948c50869
Luiz Henrique <luiz.h.santos.dev@gmail.com>
2026-06-09 21:21:48 -0300

    feat(answer): implement edit answer use case and tests

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -3,6 +3,7 @@ import { Question } from '../../enterprise/entities/question.js'
 export interface QuestionRepository {
   findById(id: string): Promise<Question | null>
   findBySlug(slug: string): Promise<Question | null>
+  save(question: Question): Promise<void>
   create(question: Question): Promise<void>
   delete(question: Question): Promise<void>
 }
---
+++ src/domain/forum/application/use-cases/edit-question.spec.ts
@@ -0,0 +1,53 @@
+import { makeQuestion } from '@/test/factories/make-question.js'
+import { InMemoryQuestionRepository } from '@/test/repositores/in-memory-question-repository.js'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id.js'
+import { EditQuestionUseCase } from './edit-question.js'
+
+let inMemoryQuestionRepository: InMemoryQuestionRepository
+let sut: EditQuestionUseCase
+
+describe('Edit Question', () => {
+  beforeEach(() => {
+    inMemoryQuestionRepository = new InMemoryQuestionRepository()
+    sut = new EditQuestionUseCase(inMemoryQuestionRepository)
+  })
+
+  it('should be able to edit a question', async () => {
+    const newQuestion = makeQuestion(
+      { authorId: new UniqueEntityId('author-1') },
+      new UniqueEntityId('question-1'),
+    )
+
+    await inMemoryQuestionRepository.create(newQuestion)
+
+    await sut.execute({
+      questionId: newQuestion.id.toValue(),
+      authorId: 'author-1',
+      title: 'Pergunta teste',
+      content: 'Conteúdo teste',
+    })
+
+    expect(inMemoryQuestionRepository.items[0]).toMatchObject({
+      title: 'Pergunta teste',
+      content: 'Conteúdo teste',
+    })
+  })
+
+  it('should not be able to edit a question from another user', async () => {
+    const newQuestion = makeQuestion(
+      { authorId: new UniqueEntityId('author-1') },
+      new UniqueEntityId('question-1'),
+    )
+
+    await inMemoryQuestionRepository.create(newQuestion)
+
+    expect(() => {
+      return sut.execute({
+        questionId: newQuestion.id.toValue(),
+        authorId: 'author-2',
+        title: 'Pergunta teste',
+        content: 'Conteúdo teste',
+      })
+    }).rejects.toBeInstanceOf(Error)
+  })
+})
